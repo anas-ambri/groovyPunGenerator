3729f4e7459f2e1589341082e2a7855dc402af41
Anas Ambri <anas.ambri@gmail.com>
2014-09-25 10:53:45 -0400

    Fix layout, add Json parser code

--- AppWithGradleTemplate/src/main/java/com/pun/generator/MainActivity.groovy
+++ AppWithGradleTemplate/src/main/java/com/pun/generator/MainActivity.groovy
@@ -4,6 +4,7 @@ import android.app.Activity;
 import android.os.Bundle
 import android.view.View
 import android.widget.TextView
+import android.widget.Toast
 import com.loopj.android.http.AsyncHttpClient
 import com.loopj.android.http.AsyncHttpResponseHandler
 import com.loopj.android.http.ResponseHandlerInterface
@@ -12,6 +13,7 @@ import org.apache.http.Header;
 
 public class MainActivity extends Activity
 {
+    private static Random random = new Random()
     private static final String API = "https://www.kimonolabs.com/api/6onwyw9k?apikey=ABzJzLoLJOSq1zVKAswwsxnUwUY7kDSv";
     private static final AsyncHttpClient client = new AsyncHttpClient();
     private TextView punText;
@@ -33,7 +35,8 @@ public class MainActivity extends Activity
 
         @Override
         void onSuccess(int i, Header[] headers, byte[] bytes) {
-            callback.onSuccess(new JsonSlurper().parse(bytes, "utf-8"))
+            def response = new JsonSlurper().parse(bytes, "utf-8")
+            callback.onSuccess(response.results.collection1)
         }
 
         @Override
@@ -50,16 +53,29 @@ public class MainActivity extends Activity
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
         punText = (TextView) findViewById(R.id.pun);
+        this.getTitles(new Callback<List>() {
 
+            @Override
+            void onSuccess(List payload) {
+                puns = payload;
+                Toast.makeText(MainActivity.this, "Titles loaded", Toast.LENGTH_SHORT).show()
+            }
+
+            @Override
+            void onFailure(String reason) {
+                Toast.makeText(MainActivity.this, "Could not load title " + reason, Toast.LENGTH_SHORT).show()
+            }
+        })
     }
 
-    public void newPun(View v) {
-        if(punText) {
-
+    public void newTitle(View v) {
+        if(punText && puns.size() > 0) {
+            def randomInt = random.nextInt(puns.size())
+            punText.setText(puns[randomInt].pun.text)
         }
     }
 
-    public static void getPuns(Callback callback) {
-        client.get(this, API, new ResponseHandler(callback));
+    public void getTitles(Callback callback) {
+        client.get(this, API, new ResponseHandler(callback))
     }
 }
